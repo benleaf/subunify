8e7736fd2c8c4a4212bf1094e39aaa19f72bcc28
Ben Leaf <ben.leaf@hotmail.com>
2025-03-04 23:43:22 -0500

    Added more styles to landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,7 +5,7 @@ import FloatingGlassCircle from "@/components/glassmorphism/FloatingGlassCircle"
 import GlassSpace from "@/components/glassmorphism/GlassSpace";
 import DynamicStack from "@/components/glassmorphism/DynamicStack";
 import GlassIconText from "@/components/glassmorphism/GlassIconText";
-import { Article, Backup, BallotOutlined, BorderTop, CreateNewFolder, Dashboard } from "@mui/icons-material";
+import { Article, Backup, BallotOutlined, BarChart, BorderTop, CreateNewFolder, Dashboard, PieChart } from "@mui/icons-material";
 
 const LandingPage = () => {
     return <div>
@@ -19,44 +19,50 @@ const LandingPage = () => {
                 <FloatingGlassCircle offset={{ bottom: '-10em', left: '-4em' }} size="medium" />
                 <GlassCard marginSize="small">
                     <div style={{ maxWidth: 400, height: 500, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                        <GlassSpace size={"tiny"}>
-                            <GlassText size="large">CREATED FROM EXCEL:</GlassText>
+                        <GlassSpace size={"tiny"} style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                            <GlassText size="large">FROM EXCEL:</GlassText>
                             <GlassText size="modrate">
-                                <Stack spacing={2} margin='1em'>
+                                <Stack spacing={3} margin='1em'>
                                     <GlassIconText size={"modrate"} icon={<Article color="primary" fontSize="medium" />}>
                                         Use any excel file that you want to collect data from
                                     </GlassIconText>
                                     <GlassIconText size={"modrate"} icon={<BorderTop color="primary" fontSize="medium" />}>
-                                        Select header rows and columns along with corresponding records,
+                                        Select your tables headder and body
                                     </GlassIconText>
                                     <GlassIconText size={"modrate"} icon={<Backup color="primary" fontSize="medium" />}>
-                                        Once everything is selected deploy to a secure server
+                                        Deploy to a secure cloud server
                                     </GlassIconText>
                                     <GlassIconText size={"modrate"} icon={<BallotOutlined color="primary" fontSize="medium" />}>
-                                        Enjoy quick and painless data access, creation and deletion using the power of web forms
+                                        Enjoy painless data access using the power of web forms
+                                    </GlassIconText>
+                                    <GlassIconText size={"modrate"} icon={<BarChart color="primary" fontSize="medium" />}>
+                                        Create charts effortlessly using your own web data
                                     </GlassIconText>
                                 </Stack>
                             </GlassText>
+                            <Button startIcon={<CreateNewFolder />} href="/excel-importer" variant="contained" style={{ color: 'white' }}>Create From Excel</Button>
                         </GlassSpace>
-                        <Button startIcon={<CreateNewFolder />} href="/excel-importer" variant="contained" style={{ color: 'white' }}>Create From Excel</Button>
                     </div>
                 </GlassCard>
                 <Divider orientation="vertical" flexItem><GlassText size="modrate">OR</GlassText></Divider>
                 <GlassCard marginSize="small">
                     <div style={{ maxWidth: 400, height: 500, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                        <GlassSpace size={"tiny"}>
-                            <GlassText size="large">CREATED FROM SCRATCH:</GlassText><GlassText size="modrate">
-                                <Stack spacing={2} margin='1em'>
+                        <GlassSpace size={"tiny"} style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                            <GlassText size="large">FROM SCRATCH:</GlassText><GlassText size="modrate">
+                                <Stack spacing={3} margin='1em'>
                                     <GlassIconText size={"modrate"} icon={<BallotOutlined color="primary" fontSize="medium" />}>
                                         Start with a blank dashboard and create tables manually
                                     </GlassIconText>
                                     <GlassIconText size={"modrate"} icon={<Backup color="primary" fontSize="medium" />}>
-                                        If at a future point you wish to upload an excel file with data, you can do this at any point
+                                        Add tables from Excel at any point
+                                    </GlassIconText>
+                                    <GlassIconText size={"modrate"} icon={<PieChart color="primary" fontSize="medium" />}>
+                                        Start right and structure your data how you want
                                     </GlassIconText>
                                 </Stack>
                             </GlassText>
+                            <Button startIcon={<Dashboard />} href="/dashboard" variant="contained" style={{ color: 'white' }}>Create Dashboard</Button>
                         </GlassSpace>
-                        <Button startIcon={<Dashboard />} href="/dashboard" variant="contained" style={{ color: 'white' }}>Create Dashboard</Button>
                     </div>
                 </GlassCard>
                 <FloatingGlassCircle offset={{ top: '-2em', right: '-2em' }} size="small" />
