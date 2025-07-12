89836d7547c01a9438b739054541cc71e1abf380
Ben Leaf <ben.leaf@hotmail.com>
2025-07-12 15:04:25 -0400

    Fixed mui imports

--- src/main.tsx
+++ src/main.tsx
@@ -8,16 +8,10 @@ import 'react-multi-carousel/lib/styles.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createTheme, CssBaseline } from '@mui/material';
-import type { } from '@mui/x-data-grid/themeAugmentation';
 import { ThemeProvider } from '@emotion/react';
 import { Colours } from './constants/Colours';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { LicenseInfo } from '@mui/x-license';
 import App from './App';
 import { UploadProvider } from './contexts/UploadContext';
-
-LicenseInfo.setLicenseKey(import.meta.env.VITE_MUI_KEY);
 
 const theme = createTheme({
   palette: {
@@ -38,12 +32,6 @@ const theme = createTheme({
       main: Colours.secondary
     },
   },
-  mixins: {
-    MuiDataGrid: {
-      pinnedBackground: Colours.lightGrey,
-      containerBackground: Colours.lightGrey,
-    },
-  },
 });
 
 declare global {
@@ -54,11 +42,9 @@ createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={theme}>
     <UploadProvider>
       <CssBaseline enableColorScheme />
-      <LocalizationProvider dateAdapter={AdapterMoment}>
-        <StrictMode>
-          <App />
-        </StrictMode>
-      </LocalizationProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
     </UploadProvider>
   </ThemeProvider>
 )
