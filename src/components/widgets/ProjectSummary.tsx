d77fbda85799c34dd1121cac83a229285a0bd483
Ben Leaf <ben.leaf@hotmail.com>
2025-07-22 14:43:21 -0400

    Fixed project summary layout

--- src/components/widgets/ProjectSummary.tsx
+++ src/components/widgets/ProjectSummary.tsx
@@ -3,13 +3,14 @@ import { IconButton, Divider, Button } from "@mui/material"
 import ColorGlassCard from "../glassmorphism/ColorGlassCard"
 import GlassText from "../glassmorphism/GlassText"
 import { useDashboard } from "@/contexts/DashboardContext"
+import { CssSizes } from "@/constants/CssSizes"
 
 const ProjectSummary = () => {
     const { properties, updateProperties } = useDashboard()
 
     return <ColorGlassCard paddingSize="small">
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap' }}>
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap', gap: CssSizes.moderate }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', flex: 2, minWidth: 300 }}>
                 <IconButton onClick={() => updateProperties({
                     page: 'projects',
                     selectedProjectId: undefined,
@@ -24,18 +25,19 @@ const ProjectSummary = () => {
                     <GlassText size="moderate">{properties.selectedProject?.description}</GlassText>
                 </div>
             </div>
-            <div style={{ flex: 1 }} />
-            <Button
-                variant="outlined"
-                startIcon={<Layers />}
-                onClick={() => updateProperties({ page: 'manageProject' })}
-            >
-                Project Actions
-            </Button>
-            <Divider orientation="vertical" style={{ height: 50, marginInline: 10 }} />
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                <GlassText size="large">{properties.selectedProject?.files.length}</GlassText>
-                <GlassText size="small">Files Uploaded</GlassText>
+            <div style={{ display: 'flex', flex: 1, justifyContent: 'end', minWidth: 300 }}>
+                <Button
+                    variant="outlined"
+                    startIcon={<Layers />}
+                    onClick={() => updateProperties({ page: 'manageProject' })}
+                >
+                    Project Actions
+                </Button>
+                <Divider orientation="vertical" style={{ height: 50, marginInline: 10 }} />
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                    <GlassText size="large">{properties.selectedProject?.files.length}</GlassText>
+                    <GlassText size="small">Files Uploaded</GlassText>
+                </div>
             </div>
         </div>
     </ColorGlassCard>
